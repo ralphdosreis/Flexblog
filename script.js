42c699f974b8d3c752a2577ca838a8dec22aceb7
ralphdosreis <ralphdosreistorres@gmail.com>
2024-02-22 23:46:11 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('li a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: "smooth",
+    // });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
