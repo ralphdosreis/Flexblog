d7aac40a2c3ef097915e5c7f7d44cc9d4a5e45c6
ralphdosreis <ralphdosreistorres@gmail.com>
2024-02-23 23:49:15 -0300

    Second commit

--- script.js
+++ script.js
@@ -10,12 +10,6 @@ function initScrollSuave() {
       block: "start",
     });
 
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: "smooth",
-    // });
   }
   linksInternos.forEach((link) => {
     link.addEventListener("click", scrollToSection);
